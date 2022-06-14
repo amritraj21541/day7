cb52d6ef4758134e247716e8ef26b88f2a724212
amrit <amrit@MSI>
2022-06-14 21:53:05 +0530

    day 7 done

---
+++ day_7/src/day_7/Demo1.java
@@ -0,0 +1,22 @@
+package day_7;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+	int a =10; 
+		
+		Integer i = Integer.valueOf(a);
+		
+		Integer k = a;
+		System.out.println(a + " "+ i + " "+ k);
+		
+
+
+	}
+
+
+
+
+	}
+
+
