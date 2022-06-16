b426936e1c86284415554f5dd4ffbfae2f159b8f
amrit <amrit@MSI>
2022-06-16 14:54:48 +0530

    day 7 done

---
+++ day_7/src/day_7/Demo2.java
@@ -0,0 +1,28 @@
+package day_7;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		{
+
+			for(int i = 1; i <= 10; i++)
+
+			{
+
+			System.out.println("before breaking = " + i );
+
+			if(i == 6)
+
+			break;
+
+			System.out.println("after breaking = " + i );
+
+			}
+
+			
+
+			
+
+	}
+
+}
---
+++ day_7/src/day_7/Demo3.java
@@ -0,0 +1,15 @@
+package day_7;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		do{
+		System.out.println(" technoserve");
+		}while(false);
+		
+		
+	
+
+	}
+
+}
